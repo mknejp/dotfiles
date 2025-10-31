7cf2a81dfc262c3892e466de0bb460c434bf4f8a
mknejp <mknejp@gmail.com>
2025-10-31 21:14:11 +0100

    nvim/init.lua

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,16 @@
+vim.o.syntax = 'on'
+vim.o.compatible = false
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.mouse = 'a'
+vim.o.signcolumn = 'yes'
+vim.o.smartindent = true
+vim.o.background = 'dark'
+vim.o.splitbelow = true
+vim.o.splitright = true
+
+vim.g.mapleader = ' '
